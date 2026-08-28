c80246e2b83a479d22946ab77ac7bb5e27d02551
Youssef Kaf <ykaf@c1r4p3.1337.ma>
2026-08-28 10:27:52 +0100

    test monitor

--- src/monitor.c
+++ src/monitor.c
@@ -2,9 +2,19 @@
 
 void *monitor_routine(void *args)
 {
+	int	i;
+	t_sumilation *simu = (t_sumilation*)args;
+
+	i = 0;
 	while (1)
 	{
+		while (i < simu->data->number_of_coders)
+		{
+			if (simu->coder[i].is_finished)
+				return ;
+		}
 
+		
 	}
 	return (NULL);
 }
@@ -12,10 +22,10 @@ void *monitor_routine(void *args)
 
 
 
-void create_monitor(t_sumilation simu)
+void create_monitor(t_sumilation *simu)
 {
 	pthread_t	monitor;
-
+	pthread_create(&monitor, NULL, monitor_routine, (void*)simu);
 
 }
 

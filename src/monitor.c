b68ea4902dc6848645403bbc06f0a7a9dd816b3c
Youssef Kaf <youssefkaf1448@gmail.com>
2026-08-28 11:14:42 +0100

    finished monitor

--- src/monitor.c
+++ src/monitor.c
@@ -8,24 +8,25 @@ void *monitor_routine(void *args)
 	long	last_compile;
 
 	i = 0;
-	current_time = get_time_of_ms();
 	while (1)
 	{
 		while (i < simu->data->number_of_coders)
 		{
+			current_time = get_time_of_ms();
 			if (simu->coder[i].is_finished)
 				return (NULL);
 			pthread_mutex_lock(&simu->state_lock);
 			last_compile = simu->coder[i].last_time_compilation;
 			pthread_mutex_unlock(&simu->state_lock);
 			if (current_time + last_compile > simu->data->time_to_burnout)
+			{
+				printf("%ld %d burned out\n", current_time - simu->start_time, simu->coder[i].id);
 				simu->is_simulation_over = 1;
+			}
 			i++;
-		}
-		usleep(500);
-		// Burnout check
-
-
+	}
+	usleep(500);
+	// Burnout check
 
 	}
 	return (NULL);
@@ -38,6 +39,6 @@ void create_monitor(t_sumilation *simu)
 {
 	pthread_t	monitor;
 	pthread_create(&monitor, NULL, monitor_routine, (void*)simu);
-
+	
 }
 

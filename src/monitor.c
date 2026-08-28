697a1250053a46da177b37ca85a1132ded838f7b
Youssef Kaf <ykaf@c1r4p3.1337.ma>
2026-08-28 10:19:41 +0100

    create monitor

---
+++ src/monitor.c
@@ -0,0 +1,21 @@
+#include	"../library/codexion.h"
+
+void *monitor_routine(void *args)
+{
+	while (1)
+	{
+
+	}
+	return (NULL);
+}
+
+
+
+
+void create_monitor(t_sumilation simu)
+{
+	pthread_t	monitor;
+
+
+}
+
